ecc12895645811d6d59b673a4cb8f5f6fa72da12
scvgong <gongmyeongyun@gmail.com>
2024-04-12 02:14:47 +0000

    add

---
+++ src/main/java/com/example/co_templates/daos/ShareDao.java
@@ -0,0 +1,17 @@
+package com.example.co_templates.daos;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;  
+@Component 
+public class ShareDao{  
+	@Autowired  
+	private SqlSessionTemplate sqlSession;  
+
+	public Object getList(String sqlMapId, Object dataMap) {
+        sqlMapId = "Home.selectByQUESTIONS_UID";
+		Object result = sqlSession.selectList(sqlMapId, dataMap);
+
+		return result;
+	}
+}
